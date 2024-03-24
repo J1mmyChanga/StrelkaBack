9e239ed9a054480025a13cedab62afec72dcc9b9
J1mmyChanga <epiakovmartynenkovv@yandex.ru>
2024-03-24 20:08:59 +0300

    init commit

--- test.py
+++ test.py
@@ -6,12 +6,14 @@ from data.categories import Categories
 db_session.global_init('db/strelka.db')
 session = db_session.create_session()
 
-pl1 = Places(id=1, title='деревня Борок', description='Круто летом отдыхать', lat=54.6669, long=35.4664, rating=4.8, category='1', image='1.jpg')
-pl2 = Places(id=2, title='парк Реадовка', description='Главное в озере не купаться', lat=54.3467, long=35.789, rating=4.2, category='2', image='2.jpg')
-pl3 = Places(id=3, title='Бункер Гитлера', description='Там вроде Гитлер застрелился', lat=54.3267, long=35.9053, rating=4.4, category='3', image='3.jpg')
+pl1 = Places(id=1, title='деревня Борок', description='Круто летом отдыхать', lat=54.767518, long=31.742566, rating=4.8, category='2', image='1.png')
+pl2 = Places(id=2, title='парк Реадовка', description='Главное в озере не купаться', lat=54.760189, long=32.020441, rating=4.2, category='2', image='2.jpg')
+pl3 = Places(id=3, title='Бункер "Медвежье Логово"', description='Бункер связистов времен ВОВ', lat=54.783998, long=31.896552, rating=4.4, category='3', image='3.jpg')
+pl4 = Places(id=4, title='парк Лопатинский', description='Самый популярный парк в Смоленске', lat=54.782508, long=32.040862, rating=4.8, category='2', image='4.jpeg')
 
 r1 = Routes(id=1, title='Исток', description='Вдоль реки Днепр', category='1', duration=50, rating=4.9)
 r2 = Routes(id=2, title='Повторение', description='Вокруг Смоленска', category='2', duration=45, rating=4.6)
+r3 = Routes(id=2, title='Путь из варяг в пельмени', description='Стань реальным петушком!', category='3', duration=228, rating=4.6)
 
 cat1 = Categories(id=1, title='Еда')
 cat2 = Categories(id=2, title='Развлечения')
@@ -20,9 +22,9 @@ cat3 = Categories(id=3, title='Туризм')
 for i in range(1, 4):
     session.add(eval(f'cat{i}'))
 
-for i in range(1, 3):
+for i in range(1, 4):
     session.add(eval(f'r{i}'))
 
-for i in range(1, 4):
+for i in range(1, 5):
     session.add(eval(f'pl{i}'))
 session.commit()
